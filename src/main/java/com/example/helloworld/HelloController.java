40438fb9ac8dd11735a47ffca93314db37fc923c
Shariq <msmshariq@users.noreply.github.com>
2025-11-26 10:49:46 +0400

    Fix hello response message

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World from Cloud Run!";
+        return "Hello World from Cloud Run.!";
     }
 
     @GetMapping("/health")
